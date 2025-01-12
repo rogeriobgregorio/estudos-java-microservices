791875fa66297eea6aa154b04e37ef70d2119303
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-11 21:58:58 -0300

    feat: Create project Human Resources Worker

---
+++ human-resources-worker/src/main/java/com/rogeriogregorio/human_resources_worker/HumanResourcesWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.human_resources_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesWorkerApplication.class, args);
+	}
+
+}
---
+++ human-resources-worker/src/test/java/com/rogeriogregorio/human_resources_worker/HumanResourcesWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.human_resources_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
