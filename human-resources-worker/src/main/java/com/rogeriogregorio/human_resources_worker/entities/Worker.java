0b37c9f27d098d0bfa0cb243bee056e897527509
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-12 13:34:11 -0300

    refactor: Refactor Worker entity class

--- human-resources-worker/src/main/java/com/rogeriogregorio/human_resources_worker/entities/Worker.java
+++ human-resources-worker/src/main/java/com/rogeriogregorio/human_resources_worker/entities/Worker.java
@@ -1,13 +1,19 @@
 package com.rogeriogregorio.human_resources_worker.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_worker")
 public class Worker implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Double dailyIncome;
