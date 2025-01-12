67befa8cf3eec1a422a3fb6c3534625b3fab81db
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-12 16:21:32 -0300

    feat: Add endpoints in  WorkerController class

--- human-resources-worker/src/main/java/com/rogeriogregorio/human_resources_worker/controllers/WorkerController.java
+++ human-resources-worker/src/main/java/com/rogeriogregorio/human_resources_worker/controllers/WorkerController.java
@@ -4,6 +4,8 @@ import com.rogeriogregorio.human_resources_worker.entities.Worker;
 import com.rogeriogregorio.human_resources_worker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +22,15 @@ public class WorkerController {
         this.workerRepository = workerRepository;
     }
 
+    @GetMapping
     public ResponseEntity<List<Worker>> getAll() {
         List<Worker> workerList = workerRepository.findAll();
         return ResponseEntity.ok(workerList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> getById(@PathVariable Long id) {
+        Worker worker = workerRepository.findById(id).orElseThrow();
+        return ResponseEntity.ok(worker);
     }
 }
