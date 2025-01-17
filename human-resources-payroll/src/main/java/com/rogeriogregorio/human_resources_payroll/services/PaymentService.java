a2c457a9568adafa48796e476f8fe5d77ced66bd
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-16 21:32:57 -0300

    refactor: Refactor PaymentService class

--- human-resources-payroll/src/main/java/com/rogeriogregorio/human_resources_payroll/services/PaymentService.java
+++ human-resources-payroll/src/main/java/com/rogeriogregorio/human_resources_payroll/services/PaymentService.java
@@ -2,34 +2,25 @@ package com.rogeriogregorio.human_resources_payroll.services;
 
 import com.rogeriogregorio.human_resources_payroll.entities.Payment;
 import com.rogeriogregorio.human_resources_payroll.entities.Worker;
+import com.rogeriogregorio.human_resources_payroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 @Service
 public class PaymentService {
 
-    @Value("${human-resources-worker.host}")
-    private String workerMicroserviceHost;
-    private final RestTemplate restTemplate;
+    private final WorkerFeignClient workerFeignClient;
 
     @Autowired
-    public PaymentService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public PaymentService(WorkerFeignClient workerFeignClient) {
+        this.workerFeignClient = workerFeignClient;
     }
 
     public Payment getPayment(long workerId, int days) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-        String url = workerMicroserviceHost + "/workers/{id}";
-
-        Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
+        Worker worker = workerFeignClient.getById(workerId).getBody();
         return new Payment(Objects.requireNonNull(worker).getName(), worker.getDailyIncome(), days);
     }
 }
