6d37ea4aaacc0feb8f0fc618233ece23d72b515a
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-19 16:19:11 -0300

    feat: Create project Human Resources Eureka Server

---
+++ human-resources-eureka-server/src/main/java/com/rogeriogregorio/human_resources_eureka_server/HumanResourcesEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.human_resources_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesEurekaServerApplication.class, args);
+	}
+
+}
---
+++ human-resources-eureka-server/src/test/java/com/rogeriogregorio/human_resources_eureka_server/HumanResourcesEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.human_resources_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
