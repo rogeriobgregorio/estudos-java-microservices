ef1b688739ac9a6ffeacc6f1ef17aa3b1a05a58e
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-18 23:46:30 -0300

    refactor: Refactor WorkerController class

--- human-resources-worker/src/main/java/com/rogeriogregorio/human_resources_worker/controllers/WorkerController.java
+++ human-resources-worker/src/main/java/com/rogeriogregorio/human_resources_worker/controllers/WorkerController.java
@@ -2,7 +2,10 @@ package com.rogeriogregorio.human_resources_worker.controllers;
 
 import com.rogeriogregorio.human_resources_worker.entities.Worker;
 import com.rogeriogregorio.human_resources_worker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +18,14 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerController {
 
+    private final Environment environment;
     private final WorkerRepository workerRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkerController.class);
+
     @Autowired
-    public WorkerController(WorkerRepository workerRepository) {
+    public WorkerController(Environment environment, WorkerRepository workerRepository) {
+        this.environment = environment;
         this.workerRepository = workerRepository;
     }
 
@@ -30,6 +37,8 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> getById(@PathVariable Long id) {
+        String port = environment.getProperty("local.server.port");
+        LOGGER.info("PORT = {}", port);
         Worker worker = workerRepository.findById(id).orElseThrow();
         return ResponseEntity.ok(worker);
     }
