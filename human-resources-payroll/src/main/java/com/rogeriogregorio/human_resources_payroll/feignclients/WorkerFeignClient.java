27003289c9ff2c5bef4ce20ae479c897f9178e32
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-18 23:47:16 -0300

    refactor: Refactor WorkerFeignClient class

--- human-resources-payroll/src/main/java/com/rogeriogregorio/human_resources_payroll/feignclients/WorkerFeignClient.java
+++ human-resources-payroll/src/main/java/com/rogeriogregorio/human_resources_payroll/feignclients/WorkerFeignClient.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "human-resources-worker", path = "/workers")
+@FeignClient(name = "human-resources-worker")
 public interface WorkerFeignClient {
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/workers/{id}")
     ResponseEntity<Worker> getById(@PathVariable Long id);
 }
