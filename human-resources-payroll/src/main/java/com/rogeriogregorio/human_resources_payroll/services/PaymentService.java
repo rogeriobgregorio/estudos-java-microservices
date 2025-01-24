b73dc47b612f8cb716567dcc1e3fee2c2ba00cd1
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-23 22:32:12 -0300

    refactor: Refactor PaymentService class

--- human-resources-payroll/src/main/java/com/rogeriogregorio/human_resources_payroll/services/PaymentService.java
+++ human-resources-payroll/src/main/java/com/rogeriogregorio/human_resources_payroll/services/PaymentService.java
@@ -3,6 +3,7 @@ package com.rogeriogregorio.human_resources_payroll.services;
 import com.rogeriogregorio.human_resources_payroll.entities.Payment;
 import com.rogeriogregorio.human_resources_payroll.entities.Worker;
 import com.rogeriogregorio.human_resources_payroll.feignclients.WorkerFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +19,14 @@ public class PaymentService {
         this.workerFeignClient = workerFeignClient;
     }
 
+    @CircuitBreaker(name = "paymentServiceCircuit", fallbackMethod = "getPaymentAlternative")
     public Payment getPayment(long workerId, int days) {
 
         Worker worker = workerFeignClient.getById(workerId).getBody();
         return new Payment(Objects.requireNonNull(worker).getName(), worker.getDailyIncome(), days);
+    }
+
+    public Payment getPaymentAlternative(Long workerId, int days, Throwable throwable) {
+        return new Payment("Fallback Worker", 400.0, days);
     }
 }
