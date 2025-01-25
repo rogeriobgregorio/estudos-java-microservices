a64385017298615bc29d717a6fc673a46f11d664
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-24 23:32:57 -0300

    update: Update application.properties

--- human-resources-payroll/src/main/java/com/rogeriogregorio/human_resources_payroll/feignclients/WorkerFeignClientFallback.java
+++ human-resources-payroll/src/main/java/com/rogeriogregorio/human_resources_payroll/feignclients/WorkerFeignClientFallback.java
@@ -9,10 +9,8 @@ import org.springframework.stereotype.Component;
 public class WorkerFeignClientFallback implements WorkerFeignClient {
     @Override
     public ResponseEntity<Worker> getById(Long id) {
-        // Criando um Worker de fallback
         Worker fallbackWorker = new Worker(id, "Fallback Worker", 0.0);
 
-        // Retornando um ResponseEntity com o Worker de fallback e um status 503 (Service Unavailable)
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(fallbackWorker);
     }
 }
